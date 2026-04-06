95b29569de4e33df8334f14a64ddb413bb4ab434
yogesh1priyadarshi <yogesh.priyadarshi@krishlabs.in>
2026-04-06 19:19:55 +0530

    update many change done

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,11 +5,14 @@ import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
   const [isLoginOpen, setIsLoginOpen] = useState(true);
+  const [isLogin, setIsLogin] = useState(true);
   const router = useRouter();
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <LoginModal
         isOpen={isLoginOpen}
+        onLogin={() => {
+          setIsLogin(true)}}
         onClose={() => {
           setIsLoginOpen(false);
           router.push("/");
